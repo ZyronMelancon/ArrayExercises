652b47464c40481452f9a893963bfaec5a7a52df
Zyron Melancon <altausolaris@gmail.com>
2016-08-24 17:23:50 -0500

    Complete
    All exercises are completed and run smoothly.

--- ArrayExercises/Source.cpp
+++ ArrayExercises/Source.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<string>
+#include<sstream>
+
 using namespace std;
 
 /*
@@ -199,34 +202,74 @@ int exerciseEight()
 int exerciseNine()
 {
 	int dragonfood[3][7];
-	double foodsum[4];
+	int t;
+	string n;
+	int foodsum[4] = { 0, 0, 0, 0 } ;
+	string dragonname[3];
 
 	for (int x = 0; x < 3; x++)
 	{
 		for (int y = 0; y < 7; y++)
 		{
-			dragonfood[x][y] = rand() % 23 + 6;
+			dragonfood[x][y] = rand() % 30 + 1;
 			foodsum[3] = foodsum[3] + dragonfood[x][y];
 		}
 	}
 
-	for (int x = 0; x < 3; x++)
+	for (int y = 0; y < 7; y++)
+		foodsum[0] = foodsum[0] + dragonfood[0][y];
+	for (int y = 0; y < 7; y++)
+		foodsum[1] = foodsum[1] + dragonfood[1][y];
+	for (int y = 0; y < 7; y++)
+		foodsum[2] = foodsum[2] + dragonfood[2][y];
+
+	for (int i = 0; i < 3; i++)
 	{
-		for (int y = 0; y < 7; y++)
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				foodsum[i] = foodsum[i] + dragonfood[x][y];
-			}
-		}
+		cout << "Enter a name for dragon #" << i + 1 << ": ";
+		cin >> dragonname[i];
 	}
 
 	cout << "Average amount of food eaten per day by all dragons: " << foodsum[3] / 21 << endl;
 
-	for (int x = 0; x < 4; x++)
+	for (int x = 0; x < 3; x++)
 	{
-		cout << "Average amount of food eaten per day by dragon #" << x + 1 << ": " << foodsum[x] << endl;
+		cout << "Average amount of food eaten per day by " << dragonname[x] << ": " << foodsum[x] / 7 << endl;
 	}
+
+	for (int x = 0; x < 3; x++)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			for (int y = 0; y < 6; y++)
+			{
+				if (dragonfood[x][y] < dragonfood[x][y + 1])
+				{
+					t = dragonfood[x][y];
+					dragonfood[x][y] = dragonfood[x][y + 1];
+					dragonfood[x][y + 1] = t;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			if (dragonfood[x][0] < dragonfood[x + 1][0])
+			{
+				t = dragonfood[x][0];
+				dragonfood[x][0] = dragonfood[x + 1][0];
+				dragonfood[x + 1][0] = t;
+				n = dragonname[x];
+				dragonname[x] = dragonname[x + 1];
+				dragonname[x + 1] = n;
+			}
+		}
+	}
+
+	cout << "The greatest amount of food eaten during a day is " << dragonfood[0][0] << " by " << dragonname[0] << endl;
+	cout << "The lowest amount of food eaten during a day is " << dragonfood[2][6] << " by " << dragonname[2] << endl;
 
 	system("pause");
 	system("cls");
@@ -241,4 +284,5 @@ int main()
 	exerciseFive();
 	exerciseSix();
 	exerciseEight();
+	exerciseNine();
 }
